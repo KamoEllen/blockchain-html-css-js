33d2467bc09ef3e7af71be87fe1f6cdd55b6d4ba
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-12-11 14:02:50 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blockchain Website</title>
+	<style>
+		/* Add some styling for the website here */
+	</style>
+</head>
+<body>
+	<header>
+		<h1>Blockchain Website</h1>
+		<nav>
+			<ul>
+				<li><a href="about.html">About</a></li>
+				<li><a href="finance-advice-blog.html">Finance Advice Blog</a></li>
+				<li><a href="contact-us.html">Contact Us</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<!-- Add the main content for the homepage here -->
+	</main>
+	<footer>
+		<!-- Add a footer for the website here -->
+	</footer>
+</body>
+</html>
